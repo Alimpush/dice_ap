054a95e0ea55251d7289b45b8247eaad2bc15674
Alimpush <alimpush17@gmail.com>
2023-12-19 23:56:00 +0600

    new changes

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -10,6 +10,10 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text(""),
+      ),
+    );
   }
 }
